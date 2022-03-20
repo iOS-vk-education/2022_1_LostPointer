76718803cd07cbbb15a5ba4510993c124350d73b
Leonid Belyaev <l.belyaev@vk.team>
2022-03-20 21:13:05 +0300

    Initial commit

---
+++ LostPointer/Controllers/MainController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class MainController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+        view.backgroundColor = .white
+    }
+    
+    private func setupConstraints() {
+    }
+    
+    private func setupViews() {
+        
+    }
+}
---
+++ LostPointer/Models/ArtistModel.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ArtistModel {
+    var name: String
+    var photo: String
+    
+    init(name: String, photo: String) {
+        self.name = name
+        self.photo = photo
+    }
+}
