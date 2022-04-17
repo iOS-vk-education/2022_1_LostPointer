6777be22701a57bb48a50d4768e81b0cd1434723
Leonid <belyaevleonid2000@mail.ru>
2022-04-17 19:38:31 +0300

    Fixed line lengths

--- LostPointer/Controllers/TabController.swift
+++ LostPointer/Controllers/TabController.swift
@@ -13,21 +13,30 @@ class TabController: UITabBarController, UITabBarControllerDelegate {
         super.viewWillAppear(animated)
         let home = HomeController()
         // Иконки тут - https://developer.apple.com/sf-symbols/
-        home.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
+        home.tabBarItem = UITabBarItem(title: "Home",
+                                       image: UIImage(systemName: "house"),
+                                       selectedImage: UIImage(systemName: "house.fill"))
 
         let favorites = FavoritesController()
-        favorites.tabBarItem = UITabBarItem(title: "Favorites", image: UIImage(systemName: "heart"), selectedImage: UIImage(systemName: "heart.fill"))
+        favorites.tabBarItem = UITabBarItem(title: "Favorites",
+                                            image: UIImage(systemName: "heart"),
+                                            selectedImage: UIImage(systemName: "heart.fill"))
 
         let search = SearchController()
-        search.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass.circle"), selectedImage: UIImage(systemName: "magnifyingglass.circle.fill"))
+        search.tabBarItem = UITabBarItem(title: "Search",
+                                         image: UIImage(systemName: "magnifyingglass.circle"),
+                                         selectedImage: UIImage(systemName: "magnifyingglass.circle.fill"))
 
         let profile = ProfileController()
-        profile.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.crop.circle"), selectedImage: UIImage(systemName: "person.crop.circle.fill"))
+        profile.tabBarItem = UITabBarItem(title: "Profile",
+                                          image: UIImage(systemName: "person.crop.circle"),
+                                          selectedImage: UIImage(systemName: "person.crop.circle.fill"))
 
         self.viewControllers = [home, favorites, search, profile]
     }
 
-    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) {
+    func tabBarController(_ tabBarController: UITabBarController,
+                          shouldSelect viewController: UIViewController) {
         self.navigationController?.setViewControllers([viewController], animated: true)
         }
 }
