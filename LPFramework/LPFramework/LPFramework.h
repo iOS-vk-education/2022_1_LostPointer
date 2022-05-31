971de81a0d1a0b8ec42002b90362b56918c04884
Leonid Belyaev <belyaevleonid2000@mail.ru>
2022-06-01 00:40:26 +0300

    Added empty framework

---
+++ LPFramework/LPFramework/LPFramework.h
@@ -0,0 +1,18 @@
+//
+//  LPFramework.h
+//  LPFramework
+//
+//  Created by Леонид Беляев on 01.06.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LPFramework.
+FOUNDATION_EXPORT double LPFrameworkVersionNumber;
+
+//! Project version string for LPFramework.
+FOUNDATION_EXPORT const unsigned char LPFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LPFramework/PublicHeader.h>
+
+
