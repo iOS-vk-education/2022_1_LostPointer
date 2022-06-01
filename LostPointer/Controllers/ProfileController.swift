5bf7bbbb2e00260f58dce95299dfa9267f0101ef
Leonid Belyaev <belyaevleonid2000@mail.ru>
2022-06-01 15:17:55 +0300

    Proper logout

--- LostPointer/Controllers/ProfileController.swift
+++ LostPointer/Controllers/ProfileController.swift
@@ -2,7 +2,7 @@ import UIKit
 import WebKit
 import LPFramework
 
-final class ProfileController: UIViewController {
+final class ProfileController: UIViewController, WKNavigationDelegate {
     var player: AudioPlayer
     var webView: WKWebView
 
@@ -16,6 +16,13 @@ final class ProfileController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    func webView(_ webView: WKWebView, didReceiveServerRedirectForProvisionalNavigation navigation: WKNavigation!) {
+        debugPrint("WebView")
+        if let url = webView.url?.absoluteString {
+            print("url = \(url)")
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.addSubview(webView)
@@ -24,6 +31,22 @@ final class ProfileController: UIViewController {
         webView.configuration.websiteDataStore.httpCookieStore.setCookie(cookie)
         webView.load(URLRequest.init(url: URL(string: "\(Constants.baseUrl)/profile")!))
         view = webView
+        navigationController?.isNavigationBarHidden = false
+        // Add observer
+        webView.addObserver(self, forKeyPath: "URL", options: .new, context: nil)
+    }
+
+    // Observe value
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        if let key = change?[NSKeyValueChangeKey.newKey] {
+
+            if let path = (key as? URL)?.path {
+                if path == "/" {
+                    navigationController?.isNavigationBarHidden = true
+                    self.navigationController?.pushViewController(SigninController(player: player), animated: false)
+                }
+            }
+        }
     }
 
     override func viewDidLayoutSubviews() {
